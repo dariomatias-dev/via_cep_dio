4eba049e85c99af0b7351a54d69941a4d44353aa
dario <matiasdario75@gmail.com>
2023-09-23 16:06:31 -0300

    feat(HomeScreen): finished zip code search input
    - Input responsible for receiving the zip code for research has been completed.
    - Added value validation.
    - Customized styles of different states.

--- lib/src/screens/home_screen/components/header_widget/cep_search_widget.dart
+++ lib/src/screens/home_screen/components/header_widget/cep_search_widget.dart
@@ -1,29 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
-class CEPSearchWidget extends StatelessWidget {
+class CEPSearchWidget extends StatefulWidget {
   const CEPSearchWidget({super.key});
 
   @override
+  State<CEPSearchWidget> createState() => _CEPSearchWidgetState();
+}
+
+class _CEPSearchWidgetState extends State<CEPSearchWidget> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _cepFieldController = TextEditingController();
+  String cepValue = '';
+
+  OutlineInputBorder borderStyle(Color color) => OutlineInputBorder(
+        borderSide: BorderSide(
+          color: color,
+        ),
+      );
+
+  final maskFormatter = MaskTextInputFormatter(
+    mask: '#####-###',
+    filter: {"#": RegExp(r'[0-9]')},
+  );
+
+  @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      style: const TextStyle(
-        color: Colors.white,
-      ),
-      decoration: InputDecoration(
-        hintText: '58140-000',
-        hintStyle: TextStyle(
-          color: Colors.grey.shade700,
+    return Form(
+      key: _formKey,
+      child: TextFormField(
+        controller: _cepFieldController,
+        inputFormatters: [maskFormatter],
+        maxLength: 9,
+        style: const TextStyle(
+          color: Colors.white,
         ),
-        enabledBorder: const OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Colors.grey,
+        decoration: InputDecoration(
+          hintText: '58140-000',
+          hintStyle: TextStyle(
+            color: Colors.grey.shade700,
           ),
+          enabledBorder: borderStyle(Colors.grey),
+          focusedBorder: borderStyle(Colors.white),
+          focusedErrorBorder: borderStyle(Colors.red),
+          errorBorder: borderStyle(Colors.red),
         ),
-        focusedBorder: const OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Colors.white,
-          ),
-        ),
+        onEditingComplete: () {
+          if (_formKey.currentState!.validate()) {
+            cepValue = _cepFieldController.text;
+          }
+        },
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'Insirá um CEP';
+          } else if (value.length < 9) {
+            return 'Insirá um CEP válido';
+          }
+
+          return null;
+        },
       ),
     );
   }

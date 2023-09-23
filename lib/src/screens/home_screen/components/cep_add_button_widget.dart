33343bb8bc1038b7396d3e4c07914c318d06b9fd
dario <matiasdario75@gmail.com>
2023-09-23 18:13:00 -0300

    feat(HomeScreen): button to add new componentized zip code
    - Componentized add new zip code button, aiming to facilitate future additions, maintenance and code reading.

---
+++ lib/src/screens/home_screen/components/cep_add_button_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class CepAddButtonWidget extends StatelessWidget {
+  const CepAddButtonWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.maxFinite,
+      height: 60.0,
+      padding: const EdgeInsets.symmetric(
+        horizontal: 10.0,
+      ),
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.black,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+        ),
+        child: const Text(
+          'Adicionar novo CEP',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/src/screens/home_screen/home_screen.dart
+++ lib/src/screens/home_screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:via_cep_dio/src/screens/home_screen/components/cep_add_button_widget.dart';
 import 'package:via_cep_dio/src/screens/home_screen/components/cep_list_widget.dart';
 import 'package:via_cep_dio/src/screens/home_screen/components/header_widget/header_widget.dart';
 
@@ -13,35 +14,13 @@ class HomeScreen extends StatelessWidget {
         toolbarHeight: 0,
         backgroundColor: Colors.black,
       ),
-      body: SafeArea(
+      body: const SafeArea(
         child: SingleChildScrollView(
           child: Column(
             children: [
-              const HeaderWidget(),
-              const CepListWidget(),
-              Container(
-                width: double.maxFinite,
-                height: 60.0,
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 10.0,
-                ),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.black,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
-                  child: const Text(
-                    'Adicionar novo CEP',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                    ),
-                  ),
-                ),
-              ),
+              HeaderWidget(),
+              CepListWidget(),
+              CepAddButtonWidget(),
             ],
           ),
         ),

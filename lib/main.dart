eeb616930ec1b6bdab008efc755af56426791fb1
dario <matiasdario75@gmail.com>
2023-09-23 10:58:41 -0300

    feat: dotenv installed and configured
    - The dotenv package, used for using environment variables, was installed and configured.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:via_cep_dio/src/via_cep_dio.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
+
   runApp(const ViaCepDio());
 }

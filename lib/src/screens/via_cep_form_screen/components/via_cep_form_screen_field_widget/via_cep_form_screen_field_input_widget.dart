d04e9491518f165be9b5341466c96bc0ad2753da
dario <matiasdario75@gmail.com>
2023-09-30 08:17:33 -0300

    feat(ViaCepFormScreen): componentized form field input
    - Input from the componentized form field, aiming to facilitate future additions and maintenance, in addition to reading the code.

---
+++ lib/src/screens/via_cep_form_screen/components/via_cep_form_screen_field_widget/via_cep_form_screen_field_input_widget.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:via_cep_dio/src/models/form_field_property_model.dart';
+
+import 'package:via_cep_dio/src/screens/via_cep_form_screen/components/via_cep_form_screen_field_widget/validate_value.dart';
+
+import 'package:via_cep_dio/src/utils/input_border_style.dart';
+
+class ViaCepFormFieldInputWidget extends StatefulWidget {
+  const ViaCepFormFieldInputWidget({
+    super.key,
+    required this.formFieldProperties,
+    required this.getFormFieldProperties,
+  });
+
+  final FormFieldPropertyModel formFieldProperties;
+  final List<FormFieldPropertyModel> getFormFieldProperties;
+
+  @override
+  State<ViaCepFormFieldInputWidget> createState() =>
+      _ViaCepFormFieldInputWidgetState();
+}
+
+class _ViaCepFormFieldInputWidgetState
+    extends State<ViaCepFormFieldInputWidget> {
+  final ValueNotifier<String?> fieldErrorNotifier =
+      ValueNotifier<String?>(null);
+
+  FormFieldPropertyModel get formFieldProperties => widget.formFieldProperties;
+
+  List<TextInputFormatter> get _inputFormatters => [
+        if (formFieldProperties.mask != null) formFieldProperties.mask!,
+        if (formFieldProperties.inputFormatter != null)
+          formFieldProperties.inputFormatter!,
+      ];
+
+  String? _validateFormFieldValue({bool shouldUpdateValidValue = true}) {
+    final FormFieldPropertyModel(
+      :setValidValue,
+    ) = formFieldProperties;
+
+    final validationResult = validateValue(formFieldProperties);
+
+    if (shouldUpdateValidValue) setValidValue(validationResult == null);
+
+    fieldErrorNotifier.value = validationResult;
+    return validationResult;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final FormFieldPropertyModel(
+      :fieldTitle,
+      :hintText,
+      :fieldController,
+      :maxLength,
+      :keyboardType,
+      :onChanged,
+      :handleOnDone,
+    ) = formFieldProperties;
+
+    return ListenableBuilder(
+      listenable: fieldErrorNotifier,
+      builder: (context, child) {
+        return TextFormField(
+          controller: fieldController,
+          focusNode: formFieldProperties.inputFocusNode,
+          inputFormatters:
+              _inputFormatters.isNotEmpty ? _inputFormatters : null,
+          keyboardType: keyboardType,
+          maxLength: maxLength,
+          decoration: InputDecoration(
+            contentPadding: const EdgeInsets.symmetric(
+              vertical: 18.0,
+              horizontal: 10.0,
+            ),
+            hintText: hintText,
+            errorText: fieldErrorNotifier.value,
+            enabledBorder: inputBorderStyle(Colors.grey).copyWith(
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            focusedBorder: inputBorderStyle(Colors.black).copyWith(
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            focusedErrorBorder: inputBorderStyle(Colors.red).copyWith(
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            errorBorder: inputBorderStyle(Colors.red).copyWith(
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+          ),
+          validator: (value) {
+            _validateFormFieldValue(
+              shouldUpdateValidValue: false,
+            );
+            return null;
+          },
+          onChanged: (value) {
+            if (onChanged != null) onChanged(value);
+
+            _validateFormFieldValue();
+          },
+          onEditingComplete: () {
+            final validationResult = _validateFormFieldValue();
+
+            if (validationResult == null) {
+              handleOnDone(
+                fieldTitle,
+                widget.getFormFieldProperties,
+              );
+            }
+          },
+          onTapOutside: (_) {
+            FocusManager.instance.primaryFocus?.unfocus();
+          },
+        );
+      },
+    );
+  }
+}
--- lib/src/screens/via_cep_form_screen/components/via_cep_form_screen_field_widget/via_cep_form_screen_field_widget.dart
+++ lib/src/screens/via_cep_form_screen/components/via_cep_form_screen_field_widget/via_cep_form_screen_field_widget.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 import 'package:via_cep_dio/src/models/form_field_property_model.dart';
 
-import 'package:via_cep_dio/src/screens/via_cep_form_screen/components/via_cep_form_screen_field_widget/validate_value.dart';
-
-import 'package:via_cep_dio/src/utils/input_border_style.dart';
+import 'package:via_cep_dio/src/screens/via_cep_form_screen/components/via_cep_form_screen_field_widget/via_cep_form_screen_field_input_widget.dart';
 
 class ViaCepFormFieldWidget extends StatelessWidget {
-  ViaCepFormFieldWidget({
+  const ViaCepFormFieldWidget({
     super.key,
     required this.formFieldProperties,
     required this.getFormFieldProperties,
@@ -17,39 +14,11 @@ class ViaCepFormFieldWidget extends StatelessWidget {
   final FormFieldPropertyModel formFieldProperties;
   final List<FormFieldPropertyModel> getFormFieldProperties;
 
-  final ValueNotifier<String?> fieldErrorNotifier =
-      ValueNotifier<String?>(null);
-
-  String? _validateFormFieldValue({bool shouldUpdateValidValue = true}) {
-    final FormFieldPropertyModel(
-      :setValidValue,
-    ) = formFieldProperties;
-
-    final validationResult = validateValue(formFieldProperties);
-
-    if (shouldUpdateValidValue) setValidValue(validationResult == null);
-
-    fieldErrorNotifier.value = validationResult;
-    return validationResult;
-  }
-
-  List<TextInputFormatter> get _inputFormatters => [
-        if (formFieldProperties.mask != null) formFieldProperties.mask!,
-        if (formFieldProperties.inputFormatter != null)
-          formFieldProperties.inputFormatter!,
-      ];
-
   @override
   Widget build(BuildContext context) {
     final FormFieldPropertyModel(
       :fieldTitle,
-      :hintText,
-      :fieldController,
       :isRequired,
-      :maxLength,
-      :keyboardType,
-      :onChanged,
-      :handleOnDone,
     ) = formFieldProperties;
 
     return Column(
@@ -75,62 +44,9 @@ class ViaCepFormFieldWidget extends StatelessWidget {
           ),
         ),
         const SizedBox(height: 2.0),
-        ListenableBuilder(
-          listenable: fieldErrorNotifier,
-          builder: (context, child) {
-            return TextFormField(
-              controller: fieldController,
-              focusNode: formFieldProperties.inputFocusNode,
-              inputFormatters:
-                  _inputFormatters.isNotEmpty ? _inputFormatters : null,
-              keyboardType: keyboardType,
-              maxLength: maxLength,
-              decoration: InputDecoration(
-                contentPadding: const EdgeInsets.symmetric(
-                  vertical: 18.0,
-                  horizontal: 10.0,
-                ),
-                hintText: hintText,
-                errorText: fieldErrorNotifier.value,
-                enabledBorder: inputBorderStyle(Colors.grey).copyWith(
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                focusedBorder: inputBorderStyle(Colors.black).copyWith(
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                focusedErrorBorder: inputBorderStyle(Colors.red).copyWith(
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                errorBorder: inputBorderStyle(Colors.red).copyWith(
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-              ),
-              validator: (value) {
-                _validateFormFieldValue(
-                  shouldUpdateValidValue: false,
-                );
-                return null;
-              },
-              onChanged: (value) {
-                if (onChanged != null) onChanged(value);
-
-                _validateFormFieldValue();
-              },
-              onEditingComplete: () {
-                final validationResult = _validateFormFieldValue();
-
-                if (validationResult == null) {
-                  handleOnDone(
-                    fieldTitle,
-                    getFormFieldProperties,
-                  );
-                }
-              },
-              onTapOutside: (_) {
-                FocusManager.instance.primaryFocus?.unfocus();
-              },
-            );
-          },
+        ViaCepFormFieldInputWidget(
+          formFieldProperties: formFieldProperties,
+          getFormFieldProperties: getFormFieldProperties,
         ),
       ],
     );

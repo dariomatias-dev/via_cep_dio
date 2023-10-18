b37eb116b9b5cffc20c0593e51ac6be1cf3e3e68
dario <matiasdario75@gmail.com>
2023-10-18 17:17:43 -0300

    feat: bottom navigation bar configured and added FloatingActionButton.extended to replace the add new zip code button
    - The bottom navigation bar has been implemented and configured on the MainScreen, and is ready to use.
    - The add new zip code button was replaced by FloatingActionButton.extended, as for the new design idea of ​​the application, it fits better.

--- lib/src/screens/main_screen/components/main_screen_body_content_widget/main_screen_body_content_widget/main_screen_body_content_widget.dart
+++ lib/src/screens/main_screen/components/main_screen_body_content_widget/main_screen_body_content_widget/main_screen_body_content_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:via_cep_dio/src/core/helpers/navigation_fade_transition.dart';
 import 'package:via_cep_dio/src/core/helpers/verifications_helper.dart';
 
 import 'package:via_cep_dio/src/models/via_cep_card_model.dart';
@@ -11,13 +10,11 @@ import 'package:via_cep_dio/src/notifiers/via_cep_service_notifier.dart';
 import 'package:via_cep_dio/src/providers/main_screen_inherited_widget.dart';
 
 import 'package:via_cep_dio/src/screens/main_screen/components/main_screen_body_content_widget/main_screen_body_content_widget/pagination_button_widget.dart';
-import 'package:via_cep_dio/src/screens/via_cep_form_screen/via_cep_form_screen.dart';
 import 'package:via_cep_dio/src/screens/main_screen/components/main_screen_body_content_widget/via_cep_card_widget.dart';
 
 import 'package:via_cep_dio/src/services/via_cep_service.dart';
 
 import 'package:via_cep_dio/src/widgets/custom_message_widget.dart';
-import 'package:via_cep_dio/src/widgets/default_button_widget.dart';
 
 class MainScreenBodyContentWidget extends StatefulWidget {
   const MainScreenBodyContentWidget({super.key});
@@ -33,23 +30,6 @@ class _MainScreenBodyContentWidgetState
 
   int skip = 0;
   final int _limit = 8;
-
-  Padding get _cepAddButtonWidget => Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 10.0,
-        ),
-        child: DefaultButtonWidget(
-          text: 'Adicionar novo CEP',
-          action: () {
-            navigationFadeTransition(
-              context,
-              () => const ViaCepFormScreen(
-                formType: 'creation',
-              ),
-            );
-          },
-        ),
-      );
 
   @override
   void initState() {
@@ -98,7 +78,6 @@ class _MainScreenBodyContentWidgetState
                 const CustomMessageWidget(
                   content: nullDataMessage,
                 ),
-              _cepAddButtonWidget,
             ],
           );
         },
@@ -120,32 +99,27 @@ class _MainScreenBodyContentWidgetState
         final ViaCepCardsDataModel viaCepCardsData = snapshot.data!;
 
         if (viaCepCardsData.results.isEmpty) {
-          return Column(
-            children: [
-              const SizedBox(
-                height: 100.0,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Não há nenhum CEP.',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                    Text(
-                      'Adicione um!',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                  ],
+          return const SizedBox(
+            height: 100.0,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Não há nenhum CEP.',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
-              ),
-              _cepAddButtonWidget,
-            ],
+                Text(
+                  'Adicione um!',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ],
+            ),
           );
         }
 
@@ -186,7 +160,6 @@ class _MainScreenBodyContentWidgetState
                   ),
                 ],
               ),
-            _cepAddButtonWidget,
           ],
         );
       },
--- lib/src/screens/main_screen/main_screen.dart
+++ lib/src/screens/main_screen/main_screen.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
+
+import 'package:via_cep_dio/src/core/helpers/navigation_fade_transition.dart';
 
 import 'package:via_cep_dio/src/providers/main_screen_inherited_widget.dart';
 
 import 'package:via_cep_dio/src/screens/main_screen/components/main_screen_header_widget/main_screen_header_widget.dart';
 import 'package:via_cep_dio/src/screens/main_screen/components/main_screen_body_content_widget/main_screen_body_content_widget/main_screen_body_content_widget.dart';
+import 'package:via_cep_dio/src/screens/via_cep_form_screen/via_cep_form_screen.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -13,35 +17,80 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  String cepToSearch = '';
+  int _currentIndex = 0;
+
+  String _cepToSearch = '';
 
   void _updateCepToSearch(String cep) {
-    if (cep != cepToSearch) {
+    if (cep != _cepToSearch) {
       setState(() {
-        cepToSearch = cep;
+        _cepToSearch = cep;
       });
     }
   }
 
+  final List<Widget> screenContents = [
+    const Column(
+      children: [
+        MainScreenHeaderWidget(),
+        MainScreenBodyContentWidget(),
+      ],
+    ),
+    const SizedBox(
+      height: 400.0,
+      child: Text('Olá!'),
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MainScreenInheritedWidget(
-      cepToSearch: cepToSearch,
+      cepToSearch: _cepToSearch,
       updateCepToSearch: _updateCepToSearch,
       child: Scaffold(
         appBar: AppBar(
           toolbarHeight: 0,
           backgroundColor: Colors.black,
         ),
-        body: const SafeArea(
+        body: SafeArea(
           child: SingleChildScrollView(
-            child: Column(
-              children: [
-                MainScreenHeaderWidget(),
-                MainScreenBodyContentWidget(),
-              ],
-            ),
+            child: screenContents.elementAt(_currentIndex),
           ),
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            navigationFadeTransition(
+              context,
+              () => const ViaCepFormScreen(
+                formType: 'creation',
+              ),
+            );
+          },
+          backgroundColor: Colors.black,
+          icon: const Icon(
+            Icons.add,
+          ),
+          label: const Text('Adiconar'),
+        ),
+        bottomNavigationBar: SalomonBottomBar(
+          currentIndex: _currentIndex,
+          onTap: (index) => setState(() {
+            _currentIndex = index;
+          }),
+          backgroundColor: Colors.black,
+          selectedItemColor: Colors.yellow,
+          selectedColorOpacity: 0.12,
+          unselectedItemColor: Colors.white,
+          items: [
+            SalomonBottomBarItem(
+              icon: const Icon(Icons.home),
+              title: const Text('Home'),
+            ),
+            SalomonBottomBarItem(
+              icon: const Icon(Icons.location_on),
+              title: const Text('CEPs'),
+            ),
+          ],
         ),
       ),
     );
